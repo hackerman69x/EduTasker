4df0f0f676b7c2d772ad9d51120d72cedc9f2266
hackerman69x <133666976+hackerman69x@users.noreply.github.com>
2024-12-04 18:49:04 +0800

    Update MAIN_Calendar_Display.php

--- MAIN_Calendar_Display.php
+++ MAIN_Calendar_Display.php
@@ -1,13 +1,18 @@
 <?php
+session_start();
 require 'database_connection.php';
 
-$query = "SELECT event_id, event_name AS title, event_start_date AS start, event_end_date AS end 
-          FROM calendar";
-$result = mysqli_query($con, $query);
+$user_id = $_SESSION['user_id'];
+
+$query = "SELECT event_id, event_name AS title, event_start_date AS start, event_end_date AS end
+FROM calendar 
+WHERE user_id = '$user_id'"; // Fetch the events of the user
+
+$result = mysqli_query($conn, $query);
 
 $events = [];
 while ($row = mysqli_fetch_assoc($result)) {
-    $row['color'] = '#007bff'; // Set a default highlight color
+    $row['color'] = '#007bff';  // Event color (can be customized)
     $events[] = $row;
 }
 
