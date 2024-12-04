eda7068a0fa9bf9836eeebac38cb5093fa5a4541
hackerman69x <133666976+hackerman69x@users.noreply.github.com>
2024-12-04 17:23:51 +0800

    Delete db_connection.php

--- db_connection.php
+++
@@ -1,8 +0,0 @@
-<?php
-try {
-    $db = new PDO('mysql:host=localhost;dbname=edutasker', 'root', '');
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("Error: " . $e->getMessage());
-}
-?>
