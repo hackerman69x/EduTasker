2da23bac77ae0251dc09e824120c12be73f5282b
hackerman69x <133666976+hackerman69x@users.noreply.github.com>
2024-12-04 18:49:22 +0800

    Update MAIN_Calendar_Save.php

--- MAIN_Calendar_Save.php
+++ MAIN_Calendar_Save.php
@@ -1,14 +1,20 @@
 <?php
+session_start();
 require 'database_connection.php';
 
 $event_name = $_POST['event_name'];
 $event_start_date = $_POST['event_start_date'];
 $event_end_date = $_POST['event_end_date'];
+$user_id = $_SESSION['user_id'];
 
-$insert_query = "INSERT INTO calendar (event_name, event_start_date, event_end_date) 
-                 VALUES ('$event_name', '$event_start_date', '$event_end_date')";
+if(!$user_id){
+  die(json_encode(["status"=> false,"message"=> "Session expired: re-login"])); 
+}
 
-if (mysqli_query($con, $insert_query)) {
+$insert_query = "INSERT INTO calendar (event_name, event_start_date, event_end_date, user_id) 
+                 VALUES ('$event_name', '$event_start_date', '$event_end_date', '$user_id')";
+
+if (mysqli_query($conn, $insert_query)) {
     echo json_encode(['status' => true, 'message' => 'Event added successfully']);
 } else {
     echo json_encode(['status' => false, 'message' => 'Failed to add event']);
